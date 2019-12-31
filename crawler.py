0585c5460eddfd837c4d892330e8d95f198a1fb0
Taeyoon Kim <tykimseoul@gmail.com>
2020-01-01 03:34:26 +0900

    update: put crawler on server

--- crawler.py
+++ crawler.py
@@ -4,6 +4,10 @@ import json
 import os
 from dotenv import load_dotenv
 from datetime import datetime, date
+from time import gmtime, time, strftime, sleep
+from pytz import timezone
+import schedule
+from tzlocal import get_localzone
 
 load_dotenv()
 
@@ -15,7 +19,6 @@ exg_columns = ['Inquiry', 'Standard Rate', 'Net Change', 'Cash Buy', 'Cash Sell'
 def crawl_intraday_data(code, time):
     intra_df = pd.DataFrame()
     for i in range(1, 41):
-        print("page", i)
         page_df = pd.read_html(os.getenv("INTRADAY_DATA_SOURCE_ADDRESS").format(code=code, time=time, page=i))[0]
         intra_df = intra_df.append(page_df)
     intra_df.dropna(inplace=True)
@@ -26,7 +29,6 @@ def crawl_intraday_data(code, time):
     yesterday_df.dropna(inplace=True)
     price_yesterday = yesterday_df[yesterday_df.columns[1]].iloc[1]
     intra_df['Net Change'] = intra_df['Price'] - price_yesterday
-    print(intra_df)
     return intra_df
 
 
@@ -41,14 +43,12 @@ def retrieve_intraday_data(code, date):
     result = pd.DataFrame.from_dict(data, orient='index')
     result = result[columns]
     result.reset_index(inplace=True, drop=True)
-    print(result)
     return result
 
 
 def crawl_intraday_kospi_data(time):
     kospi_df = pd.DataFrame()
     for i in range(1, 3):
-        print("Crawling kospi page", i)
         page_df = pd.read_html(os.getenv("INTRADAY_KOSPI_SOURCE_ADDRESS").format(time=time, page=i))[0]
         kospi_df = kospi_df.append(page_df)
     kospi_df.dropna(inplace=True)
@@ -59,7 +59,6 @@ def crawl_intraday_kospi_data(time):
     price_yesterday = yesterday_df[yesterday_df.columns[1]].iloc[1]
     kospi_df['Net Change'] = kospi_df['Price'] - price_yesterday
     kospi_df.reset_index(inplace=True, drop=True)
-    print(kospi_df)
     return kospi_df
 
 
@@ -74,14 +73,12 @@ def retrieve_intraday_kospi_data(date):
     result = pd.DataFrame.from_dict(data, orient='index')
     result = result[kospi_columns]
     result.reset_index(inplace=True, drop=True)
-    print(result)
     return result
 
 
 def crawl_exchange_rate():
     exg_df = pd.DataFrame()
     for i in range(1, 41):
-        print("page", i)
         page_df = pd.read_html(os.getenv("EXCHANGE_RATE_DATA_SOURCE_ADDRESS").format(page=i))[0]
         page_df.drop(page_df.columns[4:8], axis=1, inplace=True)
         page_df.columns = exg_columns
@@ -90,14 +87,12 @@ def crawl_exchange_rate():
     exg_df.sort_values(exg_df.columns[0], inplace=True)
     exg_df['Inquiry'] = pd.to_numeric(exg_df['Inquiry'].str[:-1])
     exg_df.set_index('Inquiry', inplace=True)
-    print(exg_df)
     return exg_df
 
 
-def save_exchange_data(df):
+def save_exchange_data(date, df):
     firebase = frb.FirebaseApplication(os.getenv("FIREBASE_ADDRESS"), None)
-    today = date.today().strftime("%Y/%m/%d")
-    df.apply(lambda r: firebase.post('/exchange/{date}'.format(date=today), json.loads(r.to_json())), axis=1)
+    df.apply(lambda r: firebase.post('/exchange/{date}'.format(date=date), json.loads(r.to_json())), axis=1)
 
 
 def retrieve_exchange_data(date):
@@ -106,19 +101,36 @@ def retrieve_exchange_data(date):
     result = pd.DataFrame.from_dict(data, orient='index')
     result.reset_index(inplace=True, drop=True)
     result.index = result.index + 1
-    print(result)
+    return result
 
 
-codes = json.loads(os.getenv("RELEVANT_STOCK_CODES"))
-for code in codes:
-    df = crawl_intraday_data(code, '20191226230000')
-    save_intraday_data(code, '12/26', df)
-    retrieve_intraday_data(code, '12/26')
+def crawl_every_night():
+    now = datetime.now().astimezone(timezone('Asia/Seoul'))
+    full_string = now.strftime('%Y%m%d%H%M%S')
+    date_string = now.strftime('%Y/%m/%d')
+    print("Initiating crawling: {d}".format(d=now))
+    start = time()
+    codes = json.loads(os.getenv("RELEVANT_STOCK_CODES"))
+    for code in codes:
+        df = crawl_intraday_data(code, full_string)
+        if df.empty:
+            print("Aborting crawling: empty data frame")
+            return
+        save_intraday_data(code, date_string, df)
 
-kospi_df = crawl_intraday_kospi_data('20191226230000')
-save_intraday_kospi_data('12/26', kospi_df)
-retrieve_intraday_kospi_data('12/26')
+    kospi_df = crawl_intraday_kospi_data(full_string)
+    save_intraday_kospi_data(date_string, kospi_df)
 
-exg_df = crawl_exchange_rate()
-save_exchange_data(exg_df)
-retrieve_exchange_data('2019/12/24')
+    exg_df = crawl_exchange_rate()
+    save_exchange_data(date_string, exg_df)
+    end = time()
+    print("Crawling complete: {t}".format(t=strftime("%H:%M:%S", gmtime(end - start))))
+
+
+korea_time = timezone('Asia/Seoul').localize(datetime(2020, 1, 1, 22, 0, 0))
+local_time = korea_time.astimezone(get_localzone()).time().strftime('%H:%M')
+schedule.every().day.at(local_time).do(crawl_every_night)
+
+while True:
+    schedule.run_pending()
+    sleep(1)
