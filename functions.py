d2f639667e2fdcceaeccd8e7e80f1f843ec0b64d
Taeyoon Kim <tykimseoul@gmail.com>
2020-01-02 00:27:06 +0900

    update: stock data import function

--- functions.py
+++ functions.py
@@ -4,14 +4,6 @@ from time import strftime
 import pandas as pd
 
 
-def intraday_from_csv(start, end):
-    df = pd.read_csv('kodex.csv')
-    df.dropna(inplace=True)
-    df['Time'] = df['Time'].str[:-3]
-
-    return df.loc[(df['Date'] >= start) & (df['Date'] <= end)]['End'].tolist()
-
-
 # prints formatted price
 def format_price(n):
     return ("-" if n < 0 else '') + "{0:.0f}".format(abs(n))
@@ -22,14 +14,12 @@ def format_time(t):
 
 
 # returns the vector containing stock data from a fixed file
-def get_stock_data(key, start, end):
-    # # User pandas_reader.data.DataReader to load the desired data. As simple as that.
-    # panel_data = data.DataReader(key, 'yahoo', start, end)
-    #
-    # # Getting just the adjusted closing prices. This will return a Pandas DataFrame
-    # # The index in this DataFrame is the major index of the panel_data.
-    # return panel_data['Close'].tolist()
-    data = intraday_from_csv(start, end)
+def get_stock_data(name, start, end):
+    df = pd.read_csv(name + '.csv')
+    df.dropna(inplace=True)
+    df['Time'] = df['Time'].str[:-3]
+
+    data = df.loc[(df['Date'] >= start) & (df['Date'] <= end)]['End'].tolist()
     print('data length: ', len(data))
     return data
 
